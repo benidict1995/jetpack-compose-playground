de20b0f3a5c2cfaef31ab366edd92f352170329e
Benidict Dulce <benidict@metromart.com>
2024-06-10 00:29:46 +0800

    project: training camp project.

---
+++ training-camp/app/src/main/java/com/benidict/trainingcamp/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.benidict.trainingcamp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF0195F1)
+val SecondaryBlue01 = Color(0xFF4DB5F5)
+val TertiaryBlue02 = Color(0xFFCCEAFC)
---
+++ training-camp/app/src/main/java/com/benidict/trainingcamp/ui/theme/Theme.kt
@@ -0,0 +1,29 @@
+package com.benidict.trainingcamp.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = PrimaryBlue,
+    secondary = SecondaryBlue01,
+    tertiary = TertiaryBlue02
+)
+
+@Composable
+fun TrainingcampTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = DarkColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
